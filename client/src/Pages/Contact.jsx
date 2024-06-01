dc31142e6a547df86ac56b1a975af57d778e6355
John Nguyen <johnnguyen1820@gmail.com>
2024-05-31 23:20:01 -0500

    contact us paragraph added

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -4,8 +4,16 @@ function Contact() {
   return (
     <div className='pt-20'>
       <h1 className='text-6xl text-center'>Contact Us</h1>
-        <div className='flex justify-center'>
-          <iframe className='m-4' title='form' width="640px" height="480px" src="https://forms.office.com/r/snpWBdArn0?embed=true" frameborder="0" marginwidth="1" marginheight="1" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+        <div className='flex flex-col items-center'>
+          <div className='w-10/12'>
+            <p className='mt-0 text-center text-4xl'>
+              At Divine Home Health Care, we are committed to offering the best care for our clients.
+              <br/>
+              <br/>
+              Contact us to learn more about our services and how we can assist you and your loved ones.
+            </p>
+          </div>
+          <iframe className='m-4 max-w-full' title='form' width="640px" height="480px" src="https://forms.office.com/r/snpWBdArn0?embed=true" frameborder="0" marginwidth="1" marginheight="1" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
         </div>
     </div>
   )
